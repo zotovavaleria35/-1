076bafff73973c6ea7c5602fea2c5673ecb38b4a
Zotova Valeria <zotovavaleria35@gmail.com>
2025-09-04 14:00:47 +0300

    Add files via upload

---
+++ laba1.py
@@ -0,0 +1,25 @@
+age=16
+print("age,",type(age).__name__,":",age)
+temperature=36.7
+print("temperature,",type(temperature).__name__,":",temperature)
+name="lera"
+print("name,",type(name).__name__,":",name)
+is_student=True
+print("is_student,",type(is_student).__name__,":",is_student)
+vegetables=["морква","огірок","помідор"]
+print("vegetables,",type(vegetables).__name__,":",vegetables)
+colors={"червоний","чорний","білий"}
+print("colors,",type(colors).__name__,":",colors)
+birth_date = (2009, 7, 16)
+print("birth_date,",type(birth_date).__name__,":",birth_date)
+film={"назва": "Острів проклятих"}
+print("film,",type(film).__name__,":",film)
+a=7
+b=16
+print("a+b=",a+b) #додавання
+print("a-b=",a-b) #віднімання
+print("a*b=",a*b) #множення
+print("a/b=",a/b) #ділення
+print("a%b=",a%b) #остача
+print("a**b=",a**b) #степінь
+print("a//b=",a//b) #ділення без остачі
