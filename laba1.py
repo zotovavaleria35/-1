d0cb6bd8d96586e118d292e3148bfa7909602530
Zotova Valeria <zotovavaleria35@gmail.com>
2025-09-04 14:22:14 +0300

    Update laba1.py

--- laba1.py
+++ laba1.py
@@ -1,19 +1,36 @@
+
 age=16
+#створити змінну age зі значенням 16
 print("age,",type(age).__name__,":",age)
+#Виводимр змінну,тип змінної, значення змінної
 temperature=36.7
+# створюємо змінну temperature зі значенням 36.7
 print("temperature,",type(temperature).__name__,":",temperature)
+# Виводимо назву змінної, її тип та значення
 name="lera"
+# створюємо змінну name зі значенням "lera"
 print("name,",type(name).__name__,":",name)
+# Виводимо назву змінної, її тип та значення
 is_student=True
+# створюємо змінну is_student зі значенням True
 print("is_student,",type(is_student).__name__,":",is_student)
+# Виводимо назву змінної, її тип та значення
 vegetables=["морква","огірок","помідор"]
+# створюємо список vegetables
 print("vegetables,",type(vegetables).__name__,":",vegetables)
+# Виводимо назву змінної, її тип та значення
 colors={"червоний","чорний","білий"}
+# створюємо множину colors
 print("colors,",type(colors).__name__,":",colors)
+# Виводимо назву змінної, її тип та значення
 birth_date = (2009, 7, 16)
+# створюємо кортеж birth_date
 print("birth_date,",type(birth_date).__name__,":",birth_date)
+# Виводимо назву змінної, її тип та значення
 film={"назва": "Острів проклятих"}
+# створюємо словник film
 print("film,",type(film).__name__,":",film)
+# Виводимо назву змінної, її тип та значення
 a=7
 b=16
 print("a+b=",a+b) #додавання
